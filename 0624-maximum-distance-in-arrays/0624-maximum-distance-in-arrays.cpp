e6ffc86aabf2da153f33347eba04eee974c5e56f
Anirudh-N-15 <anirudhnambiar15@gmail.com>
2024-08-16 22:45:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int minimum=arrays[0][0],maximum = arrays[0].back(),max_distance=0 ;
+
+        for(int i=0;i<arrays.size();++i)
+        {
+            max_distance = max(max_distance,abs(arrays[i].back()-minimum));
+            max_distance = max(max_distance,abs(maximum - arrays[i][0]));
+            minimum = min(minimum,arrays[i][0]);
+            maximum = max(maximum,arrays[i].back());
+        }
+
+        return max_distance ;
+
+
+
+    }
+};
