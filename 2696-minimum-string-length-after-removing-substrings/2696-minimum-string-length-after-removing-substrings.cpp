1c50d9dbe15b4fc86bf0fdebb1e643f70404d87a
Anirudh-N-15 <anirudhnambiar15@gmail.com>
2024-10-07 16:10:37 +0530

    Time: 4 ms (76.08%), Space: 11.6 MB (61.24%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int minLength(string s) {
-        bool found = true ;
+        stack <char> stk ;
 
-        while(found){
-            found = false ;
-            for(int i=0;i<s.size();++i){
-            string sub = s.substr(i,2);
-                if(sub=="AB" || sub=="CD"){
-                    s.erase(i,2);
-                    found= true ;
-                    break;
-                }
+        for(auto &c : s){
+            if(!stk.empty() && ((stk.top()=='A' && c=='B') || (stk.top()=='C' && c=='D'))){
+                stk.pop();
+            }
+            else{
+                stk.push(c);
             }
         }
-        return s.size();
+        return stk.size() ;
     }
 };
