b13922267f7bd549b2b8a39496a5c4476e904ee0
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2024-12-01 12:23:14 +0530

    Time: 3 ms (24.95%), Space: 14.8 MB (10.47%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -8,12 +8,12 @@ public:
             mp[i]++ ;
         }
 
-        if(mp[0]>1){
+        if(mp[0]>=2){
             return true ;
         }
 
         for(int i=0;i<n;++i){
-            if(mp[arr[i]*2]==1 || (!arr[i]%2 && mp[arr[i]/2]==1)){
+            if((mp[arr[i]*2] > 0 && arr[i]*2 != arr[i]) || (!arr[i]%2 && mp[arr[i]/2]>0 && arr[i]/2 != arr[i])){
                 return true ;
             }
         }
