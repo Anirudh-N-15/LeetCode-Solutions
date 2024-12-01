2dc5316458f544bc384e62da1eccecf24576e5c1
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2024-12-01 12:22:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int,int> mp ;
+
+        for(int i : arr){
+            mp[i]++ ;
+        }
+
+        if(mp[0]>1){
+            return true ;
+        }
+
+        for(int i=0;i<n;++i){
+            if(mp[arr[i]*2]==1 || (!arr[i]%2 && mp[arr[i]/2]==1)){
+                return true ;
+            }
+        }
+        return false ;
+    }
+};
