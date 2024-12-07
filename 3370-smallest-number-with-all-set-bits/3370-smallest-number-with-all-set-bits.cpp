3eb822d3cc96b8160f003a81213cdcdf62fbdf08
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2024-12-07 13:29:42 +0530

    Time: 0 ms (100%), Space: 8.3 MB (63.09%) - LeetHub

---
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int ans = n;
+
+        for(int i=1;i<=n;++i){
+           if(pow(2,i) > n){
+                ans = pow(2,i) ;
+                break;
+           }    
+        }
+
+        return ans -1 ;
+    }
+};
