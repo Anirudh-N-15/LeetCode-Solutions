9a083d3c511426097b2cbb27a0b347c5e93424b2
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2024-12-19 14:16:19 +0530

    Time: 0 ms (100%), Space: 9 MB (42.82%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -2,24 +2,13 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int n =arr.size();
-        vector<int> runsum(n);
-        vector<int> actualsum(n);
-
-        for(int i =0;i<n;++i){
-            runsum[i] = arr[i];
-            actualsum[i] = i;
-        }
-        for(int i=1;i<n;++i){
-            runsum[i] = runsum[i] + runsum[i-1]; 
-        }
-        for(int i=1;i<n;++i){
-            actualsum[i] = actualsum[i] + actualsum[i-1];
-        }
-
         int count = 0 ;
+        int sum = 0 ;
 
         for(int i=0;i<n;++i){
-            if(actualsum[i]==runsum[i]){
+            sum += arr[i] ;
+            int formulasum = (i*(i+1))/2 ;
+            if(sum == formulasum){
                 count++ ;
             }
         }
