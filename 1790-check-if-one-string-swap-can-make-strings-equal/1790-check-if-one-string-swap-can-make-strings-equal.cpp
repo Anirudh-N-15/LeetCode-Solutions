b3fcaf3b4568c84b39961c335033665e29f6e131
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-02-05 11:22:27 +0530

    Time: 3 ms (10.25%), Space: 8.1 MB (97.48%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n = s1.size(), m= s2.size();
+        if(n != m) return false ;
+        int count = 0;
+
+        vector<int> check(26,0);
+        for(int i=0;i<n;++i){
+            check[s1[i]-'a']++;
+            check[s2[i]-'a']--;
+        }
+        for(int i=0;i<26;++i){
+            if(check[i]!=0){
+                return false ;
+            }
+        }
+
+        for(int i=0;i<n;++i) {
+            if(s1[i] != s2[i]) {
+                count++ ;
+            }
+            if(count > 2){
+                return false ;
+            }
+        }
+        return true ;
+    }
+};
