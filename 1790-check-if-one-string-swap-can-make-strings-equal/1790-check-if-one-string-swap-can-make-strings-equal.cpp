5d6e76a1409125e83167e3189ac7fa56e033393c
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-02-05 11:31:29 +0530

    Time: 0 ms (100%), Space: 8.4 MB (61.87%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -6,21 +6,21 @@ public:
         int count = 0;
 
         vector<int> check(26,0);
-        for(int i=0;i<n;++i){
-            check[s1[i]-'a']++;
-            check[s2[i]-'a']--;
-        }
-        for(int i=0;i<26;++i){
-            if(check[i]!=0){
-                return false ;
-            }
-        }
 
         for(int i=0;i<n;++i) {
+            check[s1[i]-'a']++;
+            check[s2[i]-'a']--;
+
             if(s1[i] != s2[i]) {
                 count++ ;
             }
             if(count > 2){
+                return false ;
+            }
+        }
+
+        for(int i=0;i<26;++i){
+            if(check[i]!=0){
                 return false ;
             }
         }
