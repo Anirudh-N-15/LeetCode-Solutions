d074a43e961c8c93f4ac9cd98876a4b467c1d5ec
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-02-06 10:57:45 +0530

    Time: 131 ms (99.69%), Space: 27.8 MB (93.73%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
+        vector<int> products;
         int n = nums.size();
-        int count =0;
-        unordered_map<int,int> mp;
-
-        for(int i=0;i<n;++i){
-            for(int j=i+1;j<n;++j){
-                mp[nums[i]*nums[j]]++ ;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                products.push_back(nums[i] * nums[j]);
             }
         }
+        sort(products.begin(), products.end());
 
-        for(auto it :mp){
-            count += (4 * it.second * (it.second -1));
+        int count = 1;
+        int ans = 0;
+        n = products.size();
+        for (int i = 1; i < n; i++) {
+            if (products[i] == products[i - 1]) {
+                count++;
+            } else {
+                ans += count * (count - 1) * 4;
+                count = 1;
+            }
         }
-        return count ;
+        ans += count * (count - 1) * 4;
+
+        return ans;
     }
 };
