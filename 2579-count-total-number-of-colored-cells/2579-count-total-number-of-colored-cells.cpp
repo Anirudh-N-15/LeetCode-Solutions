c6e8bd7f6358a5a2974ca1fc2abaf495f007edac
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-03-05 14:29:42 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.42%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        
-        long long ans = 1;
-        int comDiff = 4 ;
-
-        for(int i=1;i<n;++i){
-            ans = ans + i*comDiff ;
-        }
-        return ans ;
-
+        return 1 + (long long )n * (n-1) * 2 ;
     }
 };
