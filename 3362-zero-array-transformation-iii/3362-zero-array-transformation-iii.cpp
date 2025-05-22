f4867a5f4c0be0ce33d643d7e31a5c0a59b3538b
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-05-22 19:36:30 +0530

    Time: 75 ms (93.01%), Space: 224.1 MB (73.73%) - LeetHub

---
+++ 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+        sort(queries.begin(), queries.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[0] < b[0];
+             });
+
+        priority_queue<int> heap;
+        vector<int> delArr(nums.size() + 1, 0);
+        int ops = 0;
+        for (int i = 0, j = 0; i < nums.size(); ++i) {
+            ops += delArr[i];
+            while (j < queries.size() && queries[j][0] == i) {
+                heap.push(queries[j][1]);
+                ++j;
+            }
+            while (ops < nums[i] && !heap.empty() && heap.top() >= i) {
+                ops += 1;
+                delArr[heap.top() + 1] -= 1;
+                heap.pop();
+            }
+            if (ops < nums[i]) {
+                return -1;
+            }
+        }
+        return heap.size();
+    }
+};
