02b9240f4d06f2c8d58d9968d25f16cba2caf653
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-05-23 13:04:26 +0530

    Time: 4 ms (48.39%), Space: 126.4 MB (98.06%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        int n = nums.size();
-        vector<int> netChange(n);
-        long long nodeSum = 0;
-
-        for(int i=0;i<n;i++) {
-            netChange[i] = (nums[i] ^ k) - nums[i] ;
-            nodeSum += nums[i] ;
-        }
-        sort(netChange.begin(),netChange.end(),greater<int>());
-
-        for(int i=1;i<n;i+=2) {
-            if(netChange[i-1] + netChange[i] >= 0) {
-                nodeSum += netChange[i-1] + netChange[i] ;
+        long long sum = 0;
+        int count = 0, positiveMin = INT_MAX, negativeMin = INT_MIN;
+            
+        for (int nodeValue : nums) {
+            int operatedNodeValue = nodeValue ^ k;
+            sum += nodeValue;
+            int netChange = operatedNodeValue - nodeValue;
+            
+            if (netChange > 0) {
+                positiveMin = min(positiveMin, netChange);
+                sum += netChange;
+                count++;
+            } else {
+                negativeMin = max(negativeMin, netChange);
             }
         }
-        return nodeSum; 
-
+        
+        // If the number of positive netChange values is even return the sum.
+        if (count % 2 == 0) {
+            return sum;
+        }
+        
+        // Otherwise return the maximum of both discussed cases.
+        return max(sum - positiveMin, sum + negativeMin);
     }
 };
