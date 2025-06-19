c619761d6de053003140946ff2d75f297b01f512
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-06-19 20:30:13 +0530

    Time: 135 ms (48.73%), Space: 17.5 MB (52.96%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
+        //Here acc to the choice diagram we can either choose an element or avoid it.
+        //If the element in nums <= sum, then we can either include it or not.
+        //That is what the or condition is for. The choice we make will check the prev
+        //one and if any one is true the dp[i][j] = true 
+
+        int sum = accumulate(nums.begin(),nums.end(),0);
         int n = nums.size();
-        int sum = 0;
-        for(int &num : nums){
-            sum += num;
+        if(sum % 2 != 0) return false ;
+
+        sum /= 2; // We only need half of the sum 
+        vector<vector<bool>> dp(n+1,vector<bool>(sum+1,false));
+
+        for(int i=0;i<=n;i++) {
+            dp[i][0] = true ;   //We can make 0 sum using 0-n elements in nums
         }
-        if((sum & 1) != 0) return false ;
 
-        int targetSum = sum/2 ;
-        vector<bool> dp(targetSum+1,false);
-        dp[0] = true ;
-
-        for(int &num : nums){
-            for(int i = targetSum;i >= num; --i){
-                dp[i] = dp[i] || dp[i - num];
-                if(dp[targetSum]) return true ;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=sum;j++) {
+                //The choice we are currently making is less than sum
+                //or not. We are solving the subproblems.
+                if(nums[i-1] <= j) { 
+                    dp[i][j] = dp[i - 1][j - nums[i -1]] || dp[i- 1][j] ;                
+                } else {
+                    dp[i][j] = dp[i - 1][j] ;
+                }
             }
         }
-        return dp[targetSum] ;
+        return dp[n][sum] ;
     }
 };
