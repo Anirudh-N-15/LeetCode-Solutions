80b76659f40d0f23330a10d401adc990658e310f
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-06-22 14:16:48 +0530

    Time: 15 ms (55.56%), Space: 26.4 MB (26.1%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        const int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum < abs(target) || (sum + target) % 2 == 1)
-            return 0;
-        return knapsack(nums, (sum + target) / 2);
+    int found(vector<int>& nums,vector<vector<int>>& dp,int i,int s,int n,int target,int sum) {
+        if(i == n && s == target) {
+            return 1;   //Found one way to reach target sum
+        }
+        if(i >= n) {
+            return 0;   //Found no way to reach target sum
+        }
+        if(dp[i][s + sum] != -1) {
+            return dp[i][s + sum] ;
+        }
+
+        return dp[i][s + sum] = found(nums,dp,i+1,s + nums[i],n,target,sum) + 
+                                found(nums,dp,i+1,s- nums[i],n,target,sum) ;
     }
 
-private:
-    int knapsack(const vector<int>& nums, int target) {
-        vector<int> dp(target + 1);
-        dp[0] = 1;
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size(),sum = 0;
+        for(const int &num : nums) {
+            sum += num ;
+        }
+        vector<vector<int>> dp(n+1,vector<int>(sum*2 +1,-1));
 
-        for (const int num : nums)
-            for (int i = target; i >= num; --i)
-                dp[i] += dp[i - num];
-
-        return dp[target];
+        return found(nums,dp,0,0,n,target,sum);
     }
 };
