249cdaeccbeee4f59d1fdfcbba535313b4ca4395
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-06-25 12:28:24 +0530

    Time: 19 ms (97.09%), Space: 26.6 MB (95.55%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,54 +1,57 @@
+//The idea here is to add both strings and then find the length of the LCS(longest common
+//subsequence) and then subtract it from the added string's length.
+//This works only for finding the length. The idea here is similar.
+//From the constructed table we backtrack to find the answer.
+//Follow the code to know how it works.
+
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.length();
-        int m = str2.length();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    string shortestCommonSupersequence(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-        for (int row = 0; row <= n; row++) {
-            dp[row][0] = row;
-        }
-        for (int col = 0; col <= m; col++) {
-            dp[0][col] = col;
-        }
-
-        for (int row = 1; row <= n; row++) {
-            for (int col = 1; col <= m; col++) {
-                if (str1[row - 1] == str2[col - 1]) {
-                    dp[row][col] = dp[row - 1][col - 1] + 1;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(s[i-1] == t[j-1]) { //We found a match
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
-                    dp[row][col] = min(dp[row - 1][col], dp[row][col - 1]) + 1;
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
+        //dp[n][m] contains the length of the LCS
 
+        int i=n,j=m;
         string ans = "";
-        int row = n, col = m;
-
-        while (row > 0 && col > 0) {
-            if (str1[row - 1] == str2[col - 1]) {
-                ans += str1[row - 1];
-                row--;
-                col--;
-            } else if (dp[row - 1][col] < dp[row][col - 1]) {
-                ans += str1[row - 1];
-                row--;
+        while(i > 0 && j > 0) {
+            if(s[i-1] == t[j-1]) {
+                //Add to string if characters are same
+                ans.push_back(s[i-1]);
+                i-- ;
+                j-- ;
+            } else if(dp[i - 1][j] > dp[i][j-1]) {
+                //If top has higher value add it to string
+                ans.push_back(s[i-1]);
+                i-- ;
             } else {
-                ans += str2[col - 1];
-                col--;
+                //If left side has higher value then take from second string
+                ans.push_back(t[j-1]);
+                j-- ;
             }
         }
 
-        while (row > 0) {
-            ans += str1[row - 1];
-            row--;
+        //Complete if there are remaining characters in the string
+        while(i > 0) {
+            ans.push_back(s[i-1]);
+            i-- ;
         }
-        while (col > 0) {
-            ans += str2[col - 1];
-            col--;
+        while(j > 0) {
+            ans.push_back(t[j-1]);
+            j-- ;
         }
 
-        reverse(ans.begin(), ans.end());
-        return ans;
+        reverse(ans.begin(),ans.end());
+        return ans ;
+
     }
 };
