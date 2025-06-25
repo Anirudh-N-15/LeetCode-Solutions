13d8bdf95b37818e4b56fa07f00c9dda0f926211
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-06-25 13:35:17 +0530

    Time: 13 ms (82.26%), Space: 25.3 MB (88.27%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -19,3 +19,30 @@ public:
         return ans ;
     }
 };
+
+//We could also use DP, just find the longest common Palindromic subsequence and then
+//subtract it from the length of the original string which will fetch us the number of
+//chars to be deleted.
+
+// class Solution {
+//   public:
+//     int minDeletions(string s) {
+//         int n = s.size();
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+//         string t = s;
+//         reverse(t.begin(),t.end());
+        
+//         for(int i=1;i<=n;i++) {
+//             for(int j=1;j<=n;j++) {
+//                 if(s[i-1] == t[j-1]) {
+//                     dp[i][j] = dp[i-1][j-1] + 1;
+//                 } else {
+//                     dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+//                 }
+//             }
+//         }
+//         return n - dp[n][n] ;
+        
+//     }
+// };
+
