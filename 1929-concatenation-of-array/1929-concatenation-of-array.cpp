6f84d418fa21d4c227cad844b591372d58c4db57
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-07-06 22:59:38 +0530

    Time: 0 ms (100%), Space: 17.2 MB (17.2%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
 
         for(int i=0;i<n;i++) {
-            nums.push_back(nums[i]);
+            nums.emplace_back(nums[i]);
         }
         return nums ;
     }
