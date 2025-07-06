2d68e92a1537dee7d816f5bdf3233266d514930c
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-07-06 22:19:11 +0530

    Time: 7 ms (96.42%), Space: 16.4 MB (96.54%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,16 +1,18 @@
+//If we don't initialise with INT_MAX we can directly avoid the if condtion
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> dp(n, vector<int> (n,INT_MAX)) ;
+        vector<vector<int>> dp(n, vector<int> (n,1e9)) ;
         int count =0;
 
+        //Kind of constructing the adj matrix
         for(auto &edge : edges){
             int u = edge[0], v = edge[1],w= edge[2] ;
             dp[u][v] = w ;
             dp[v][u] = w ;
         }
 
-
+        //Making sure there are no self loops
         for(int i=0;i<n;++i){
             dp[i][i] = 0 ;
         }
@@ -18,7 +20,6 @@ public:
         for(int k=0;k<n;++k){
             for(int i=0;i<n;++i){
                 for(int j=0;j<n;++j){
-                    if(dp[i][k]!=INT_MAX && dp[k][j]!=INT_MAX)
                     dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
                 }
             }
@@ -26,6 +27,7 @@ public:
 
         int minreachablecity = n ;
         int result = -1 ;
+
 
         for(int i=0;i<n;++i){
             int resultcount = 0 ;
@@ -36,6 +38,8 @@ public:
                 }
             }
 
+            //Because we have to return the city with the greatest number
+            //Even afer finding the city with the minimum number
             if(resultcount <= minreachablecity){
                 minreachablecity = resultcount ;
                 result = i ;
