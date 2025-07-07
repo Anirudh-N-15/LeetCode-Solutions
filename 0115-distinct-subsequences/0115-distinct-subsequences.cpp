3abe81588789c6a17366f04c987698e06b67a530
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-07-07 12:52:15 +0530

    Time: 35 ms (34.71%), Space: 26.8 MB (71.98%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,27 @@
+//For each char we have the option whether to choose it or not. Therefore, if we encounter
+//a match either choose both or dont chose. Then memoize, voila
+
+class Solution {
+public:
+    int memo(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+        if(j < 0) return 1; //We have found one match
+        if(i < 0) return 0; //We have not found a match so far
+
+        if(dp[i][j] != -1) return dp[i][j] ;
+
+        if(s[i] == t[j]) { //We have found a match now
+            return dp[i][j] = memo(i-1,j-1,s,t,dp) //We have found a match and chose both
+                            + memo(i-1,j,s,t,dp); //We didn't choose in this path
+        } else {    //No match to be found
+            return dp[i][j] = memo(i-1,j,s,t,dp); 
+        }
+    }
+
+
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+
+        return memo(n-1,m-1,s,t,dp);
+    }
+};
