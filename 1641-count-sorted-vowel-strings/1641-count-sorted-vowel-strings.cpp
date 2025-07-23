7bd219247d31b61deaade81be9344417f7700901
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-07-23 17:48:59 +0530

    Time: 0 ms (100%), Space: 9.1 MB (11.84%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
-        return recurse(n, 0, 0);
+        vector<vector<int>> dp(n+1,vector<int>(5,-1));
+        return recurse(n, 0, 0,dp);
     }
-    int recurse(int n, int size, int idx){
+    int recurse(int n, int size, int idx,vector<vector<int>>& dp){
         if(size==n)return 1;
         if(idx==5)return 0;
 
-        int take = recurse(n, size+1, idx);
-        int non_take = recurse(n, size, idx+1);
+        if(dp[size][idx] != -1) return dp[size][idx];
 
-        return take+non_take;
+        int take = recurse(n, size+1, idx,dp);
+        int non_take = recurse(n, size, idx+1,dp);
+
+        return dp[size][idx] = take+non_take;
     }
 };
