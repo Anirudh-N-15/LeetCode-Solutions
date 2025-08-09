686850262debfa34a45e727f29f2e0c651a6763c
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-08-09 10:33:45 +0530

    Time: 0 ms (100%), Space: 7.9 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        vector<long long > powers(33,1);
-        if(n == 1) return true ;
-        int i;
-        for(i=1;i<32;i++) {
-            powers[i] = powers[i-1] * 2 ;
-            if(powers[i] == n) {
-                return true ;
-            }
-        }
-        return false ;
+        //Simple trick is to check if n&(n-1) == 0. This works since n is a power of 2
+        //its set bit will be like 10000... and n-1 will be like 0111...
+        //Since you bitwise and it you will get 0 as the answer else it is not a power
+        // of 2.
+        if(n <= 0 ) return false ;
+        return (n&(n-1)) == 0 ? true : false ;
     }
 };
