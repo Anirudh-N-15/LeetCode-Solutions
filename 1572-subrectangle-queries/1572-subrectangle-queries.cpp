55ccc2e80966e5acbebd132fb6850560bffd1637
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-08-16 18:09:42 +0530

    Time: 33 ms (18.75%), Space: 25.5 MB (27.27%) - LeetHub

--- 1572-subrectangle-queries/1572-subrectangle-queries.cpp
+++ 1572-subrectangle-queries/1572-subrectangle-queries.cpp
@@ -2,13 +2,7 @@ class SubrectangleQueries {
 public:
     vector<vector<int>> ans ;
     SubrectangleQueries(vector<vector<int>>& rectangle) {
-        int n = rectangle.size(), m = rectangle[0].size();
-        ans.assign(n,vector<int>(m,0));
-        for(int i=0;i<rectangle.size();i++) {
-            for(int j=0;j<rectangle[0].size();j++) {
-                ans[i][j] = rectangle[i][j] ;
-            }
-        }
+        ans = rectangle;
     }
     
     void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
