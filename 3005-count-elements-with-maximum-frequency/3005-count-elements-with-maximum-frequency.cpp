398ca0ede726f9eaa694d51ea9ca54cabc766048
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-09-22 12:19:50 +0530

    Time: 3 ms (24.42%), Space: 23.1 MB (67.03%) - LeetHub

---
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int,int> freq ;
+        for(int num : nums) {
+            freq[num]++ ;
+        }
+        int maxi = 0;
+        for(auto it : freq) {
+            maxi = max(maxi,it.second);
+        }
+        int sum = 0;
+        for(auto it : freq) {
+            if(it.second == maxi) {
+                sum += it.second ;
+            }
+        }
+        return sum ;
+    }
+};
