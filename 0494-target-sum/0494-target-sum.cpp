d88d2909765e9f814b5e47b045de7f6b6d5c74ac
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-11-13 14:25:26 +0530

    Time: 11 ms (61.46%), Space: 26.3 MB (23.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int found(vector<int>& nums,vector<vector<int>>& dp,int i,int s,int n,int target,int sum) {
-        if(i == n && s == target) {
-            return 1;   //Found one way to reach target sum
-        }
-        if(i >= n) {
-            return 0;   //Found no way to reach target sum
-        }
-        if(dp[i][s + sum] != -1) {
-            return dp[i][s + sum] ;
+    int memo(vector<int>& nums,vector<vector<int>>& dp, int target,int s, int i,int sum) {
+        if(i < 0) {
+            if(s == target) return 1 ;
+            else return 0 ;
         }
 
-        return dp[i][s + sum] = found(nums,dp,i+1,s + nums[i],n,target,sum) + 
-                                found(nums,dp,i+1,s- nums[i],n,target,sum) ;
+        if(dp[i][s + sum] != -1) return dp[i][s + sum] ;
+
+        return dp[i][s + sum ] = memo(nums,dp,target,s + nums[i],i-1,sum) + 
+                                   memo(nums,dp,target,s - nums[i],i-1,sum);
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size(),sum = 0;
-        for(const int &num : nums) {
-            sum += num ;
-        }
+        int n = nums.size();
+        int sum = accumulate(nums.begin(),nums.end(),0);
+
         vector<vector<int>> dp(n+1,vector<int>(sum*2 +1,-1));
 
-        return found(nums,dp,0,0,n,target,sum);
+        return memo(nums,dp,target,0,n-1,sum);
     }
 };
