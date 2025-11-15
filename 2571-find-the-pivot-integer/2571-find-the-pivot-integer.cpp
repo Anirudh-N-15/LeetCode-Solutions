dd463369858ceb1880bf95dfbc20cf898aa25662
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-11-16 01:49:38 +0530

    Time: 43 ms (5.16%), Space: 8.1 MB (71.64%) - LeetHub

---
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        
+        for(int i=1;i<=n;i++) {
+            int leftSum = 0,rightSum = 0;
+            for(int j=i;j>=1;j--) {
+                leftSum += j;
+            }
+            for(int k=i;k<=n;k++) {
+                rightSum += k;
+            }
+            if(leftSum == rightSum) return i ;
+        }
+        return -1; 
+    }
+};
