0dbf3f2f32346edec0c6eea56a79563b33e0e089
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-12-17 19:01:37 +0530

    Time: 53 ms (42.66%), Space: 154 MB (5%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,29 @@
+#define offset 10000
+
 class Solution {
 public:
-    int memo(vector<int>& nums,vector<vector<int>>& dp, int target,int s, int i,int sum) {
-        if(i < 0) {
-            if(s == target) return 1 ;
-            else return 0 ;
+    int memo(vector<int>& nums,vector<vector<int>>& dp,int &target, int i,int sum) {
+        int n = nums.size() ;
+        if(i >= n) {
+            return sum == target ;
         }
 
-        if(dp[i][s + sum] != -1) return dp[i][s + sum] ;
+        if(dp[i][target+sum + offset] != -1) {
+            return dp[i][target+sum + offset] ;
+        }
 
-        return dp[i][s + sum ] = memo(nums,dp,target,s + nums[i],i-1,sum) + 
-                                   memo(nums,dp,target,s - nums[i],i-1,sum);
+        int numWays = 0;
+        numWays += memo(nums,dp,target,i+1, sum+nums[i]);
+        numWays += memo(nums,dp,target,i+1, sum-nums[i]);
+
+        return dp[i][target + sum + offset] = numWays ;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
+        int n = nums.size() ;
         int sum = accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>> dp(n+1,vector<int>(2*sum +1 + offset,-1));
 
-        vector<vector<int>> dp(n+1,vector<int>(sum*2 +1,-1));
-
-        return memo(nums,dp,target,0,n-1,sum);
+        return memo(nums,dp,target,0,0);
     }
 };
