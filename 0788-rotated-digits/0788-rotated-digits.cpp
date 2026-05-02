91b331232fbdaf92c974cb742886f3ddec22ab85
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2026-05-02 14:50:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (66.14%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    int rotatedDigits(int n) {
-        vector<bool> dp(10, false);
-        dp[2] = dp[5] = dp[6] = dp[9] = true;
-        int count = 0;
+    bool isGoodNumber(int num) {
+        bool hasValidRotate = false;
 
-        for (int i = 0; i <= n; i++) {
-            int num = i ;
-            bool flag = false ;
-            while(num > 0) {
-                int currNum = num % 10;
-                if(currNum == 3 || currNum == 4 || currNum == 7) {
-                    flag = true ;
-                }
-                num /= 10 ;
-            }
-            if(flag) {
-                continue ;
-            } else {
-                num = i ;
-                while(num > 0) {
-                    int currNum = num % 10 ;
-                    
-                    if(dp[currNum]) {
-                        count++ ;
-                        break;
-                    }
-                    num /= 10 ;
-                }
-            }
+        while (num > 0) {
+            int d = num % 10;
+
+            if (d == 3 || d == 4 || d == 7)
+                return false;
+            if (d == 2 || d == 5 || d == 6 || d == 9)
+                hasValidRotate = true;
+
+            num /= 10;
         }
-        return count ;
+
+        return hasValidRotate;
+    }
+
+    int rotatedDigits(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isGoodNumber(i)) count++;
+        }
+        return count;
     }
 };
