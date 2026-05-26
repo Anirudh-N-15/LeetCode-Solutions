3fb8ea68dc024ee79cb87cb3a3ccc9080a6b41ff
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2026-05-26 10:53:37 +0530

    Time: 8 ms (5.1%), Space: 9.1 MB (58.93%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        int count = 0;
+        set<char> st ;
+        for(const char &ch : word) {
+            st.insert(ch);
+        }
+
+        for(const char &ch : st) {
+            if(islower(ch)) {
+                cout << ch << endl ;
+                if(st.find(toupper(ch)) != st.end()) {
+                    count++ ;
+                }
+            }
+        }
+        return count ;
+    }
+};
