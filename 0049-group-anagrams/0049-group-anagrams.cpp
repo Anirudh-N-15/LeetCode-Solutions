51ade75308ba8f003d8b176e0e8951cd1b525771
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2026-06-18 12:20:28 +0530

    Time: 76 ms (6.43%), Space: 34.3 MB (10.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,45 +1,30 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        vector<bool> visited(n,false);
-        vector<vector<string>> ans;
-        sort(strs.begin(),strs.end());
+        unordered_map<string,vector<string>> umap ;
 
-        for(int i=0;i<n;++i){
-            if(visited[i] == true) continue;
-            vector<string> res ;
-            res.push_back(strs[i]);
-            visited[i] = true;
-            for(int j=i+1;j<n;++j){
-                if(valid(strs[i],strs[j]) && visited[j]!= true){
-                    res.push_back(strs[j]);
-                    visited[j] = true;
-                }
+        for(const auto &str : strs) {
+            vector<int> count(26,0) ;
+
+            for(const char &ch : str) {
+                count[ch - 'a']++ ;
             }
-            ans.push_back(res);
-        }
 
-        return ans; 
-    }
-    bool valid(string str1,string str2){
-        if(str1.size() != str2.size()) return false ;
-        int n = str1.size();
-        vector<int> count(26,0);
+            string s = "";
 
-        for(const char &c : str1){
-            count[c - 'a']++ ;
-        }
-
-        for(const char &c : str2){
-            count[c - 'a']-- ;
-        }
-
-        for(int i=0;i<26;++i){
-            if(count[i] != 0){
-                return false ;
+            for(int i = 0; i < 26; i++) {
+                s += to_string(count[i]) + '#' ;
             }
+
+            umap[s].push_back(str) ;
         }
-        return true ;
+
+        vector<vector<string>> ans ;
+
+        for( auto &[key,val] : umap) {
+            ans.push_back(val) ;
+        }
+
+        return ans ;
     }
 };
